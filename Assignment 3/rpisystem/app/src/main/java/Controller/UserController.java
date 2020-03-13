ccc8273f8e73aee7398477375b9384601c9ca8fa
Marco Martinez <mmartinezivan22@gmail.com>
2020-03-13 12:47:11 -0400

    first commits to application
    made changes that allowed the frontend app to communicate with the backend app

--- Assignment 3/rpisystem/app/src/main/java/Controller/UserController.java
+++ Assignment 3/rpisystem/app/src/main/java/Controller/UserController.java
@@ -1,41 +1,47 @@
-package Controller;
+package com.nimps.rpisystem.rpisystem.Controller;
+
 import com.nimps.rpisystem.rpisystem.Exception.ResourceNotFoundException;
 import com.nimps.rpisystem.rpisystem.User;
 import com.nimps.rpisystem.rpisystem.Repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
 
 @RestController
-@RequestMapping("/api")
 public class UserController {
 
-    @Autowired
+//    @Autowired
     UserRepo UserRepo;
 
+    public UserController(UserRepo UserRepo){
+        this.UserRepo = UserRepo;
+    }
+
     // Get All User
-    @GetMapping("/users")
+    @GetMapping("/api/users")
     public List<User> getAllUsers() {
         return UserRepo.findAll();
     }
     // Create a new User
-    @PostMapping("/users")
+    @PostMapping("/api/users")
     public User createUser(@Valid @RequestBody User user) {
         return UserRepo.save(user);
     }
+
     // Get a Single User
-    @GetMapping("/users/{id}")
+    @GetMapping("/api/users/{id}")
     public User getUserById(@PathVariable(value = "id") Long userId) {
         return UserRepo.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
     }
+
     // Update a User
-    @PutMapping("/users/{id}")
-    public User updateUser(@PathVariable(value = "id") Long userId,
-                           @Valid @RequestBody User userDetails) {
+    @PutMapping("/api/users/{id}")
+    public User updateUser(@PathVariable(value = "id") Long userId, @Valid @RequestBody User userDetails) {
 
         User user = UserRepo.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
@@ -51,7 +57,7 @@ public class UserController {
         return updatedUser;
     }
     // Delete a User
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/api/users/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable(value = "id") Long userId) {
         User user = UserRepo.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
