0455b5e379ebdd5e80e1c8443777835714663de3
Isaiah <60149476+IsaiahNw@users.noreply.github.com>
2020-04-20 15:48:01 -0400

    update

--- Assignment 3/rpisystem/app/src/Rental.js
+++ Assignment 3/rpisystem/app/src/Rental.js
@@ -51,10 +51,10 @@ export default class Rental extends React.Component{
                             <tbody>
                             { this.state.table.map(item => <tr key={item.id}>
                                 <td>{item.id}</td>
-                                <td>{item.check_in_date}</td>
+                                <td><Badge variant="light">{item.check_in_date}</Badge></td>
                                 <td>{item.checkin_by}</td>
                                 <td>{item.checkout_by}</td>
-                                <td>{item.checkout_date}</td>
+                                <td><Badge variant="secondary">{item.checkout_date}</Badge></td>
                                 <td><Badge variant="primary">{item.due_date}</Badge></td>
                                 <td>{item.kit_barcode}</td>
                                 <td>{item.student_panther_id}</td>
